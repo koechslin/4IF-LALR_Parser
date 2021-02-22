790ef39a5d9b39093975c15a32517055b829791d
koechslin <killian.oechslin@insa-lyon.fr>
2021-02-23 00:04:54 +0100

    Add comments

--- lexer.h
+++ lexer.h
@@ -1,19 +1,46 @@
+/*************************************************************************
+                           Lexer  -  Header file
+                             -------------------
+    début                : 23/02/2021
+    copyright            : (C) 2021 par Killian OECHSLIN et Thomas MIGNOT
+*************************************************************************/
+
+//---------- Interface de la classe <Lexer> (fichier lexer.h) ----------------
 #pragma once
 
+//--------------------------------------------------- Interfaces utilisées
 #include <string>
 #include "symbole.h"
+
 using namespace std;
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Lexer>
+//
+//
+//------------------------------------------------------------------------
 
 class Lexer {
-
+   //----------------------------------------------------------------- PUBLIC
    public:
-      Lexer(string s) : flux(s), tete(0), tampon(nullptr) { }
-      ~Lexer() { }
-
+      //----------------------------------------------------- Méthodes publiques
       Symbole * Consulter();
       void Avancer();
 
+      //------------------------------------------------- Surcharge d'opérateurs
+
+      //-------------------------------------------- Constructeurs - destructeur
+      Lexer(string s) : flux(s), tete(0), tampon(nullptr) { }
+      ~Lexer() { }
+
+   //------------------------------------------------------------------ PRIVE
    protected:
+      //----------------------------------------------------- Méthodes protégées
+
+      //----------------------------------------------------- Attributs protégés
       string flux;
       int tete;
       Symbole * tampon;
--- symbole.h
+++ symbole.h
@@ -1,29 +1,68 @@
+/*************************************************************************
+                           Symbole  -  Header file
+                             -------------------
+    début                : 23/02/2021
+    copyright            : (C) 2021 par Killian OECHSLIN et Thomas MIGNOT
+*************************************************************************/
+
+//---------- Interface de la classe <Symbole> (fichier symbole.h) ----------------
 #pragma once
 
+//--------------------------------------------------- Interfaces utilisées
 #include <string>
+
 using namespace std;
-
-enum Identificateurs { OPENPAR, CLOSEPAR, PLUS, MULT, INT, FIN, ERREUR };
-
+//------------------------------------------------------------- Constantes
 const string Etiquettes[] = { "OPENPAR", "CLOSEPAR", "PLUS", "MULT", "INT", "FIN", "ERREUR" };
 
+//------------------------------------------------------------------ Types
+enum Identificateurs { OPENPAR, CLOSEPAR, PLUS, MULT, INT, FIN, ERREUR };
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Symbole>
+//
+//
+//------------------------------------------------------------------------
+
+
 class Symbole {
+   //----------------------------------------------------------------- PUBLIC
    public:
-      Symbole(int i) : ident(i) {  }
-      virtual ~Symbole() { }
-      operator int() const { return ident; }
+      //----------------------------------------------------- Méthodes publiques
       virtual void Affiche();
 
+      //------------------------------------------------- Surcharge d'opérateurs
+      operator int() const { return ident; }
+
+      //-------------------------------------------- Constructeurs - destructeur
+      Symbole(int i) : ident(i) {  }
+      virtual ~Symbole() { }
+
+   //------------------------------------------------------------------ PRIVE
    protected:
+      //----------------------------------------------------- Méthodes protégées
+
+      //----------------------------------------------------- Attributs protégés
       int ident;
 };
 
 class Entier : public Symbole {
+   //----------------------------------------------------------------- PUBLIC
    public:
+      //----------------------------------------------------- Méthodes publiques
+      virtual void Affiche();
+
+      //------------------------------------------------- Surcharge d'opérateurs
+
+      //-------------------------------------------- Constructeurs - destructeur
       Entier(int v) : Symbole(INT), valeur(v) { }
       ~Entier() { }
-      virtual void Affiche();
+
+   //------------------------------------------------------------------ PRIVE
    protected:
+      //----------------------------------------------------- Méthodes protégées
+
+      //----------------------------------------------------- Attributs protégés
       int valeur;
 };
 
