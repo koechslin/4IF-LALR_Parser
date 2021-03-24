fe0621b55b190c607f8db88f81ea63d95069f5b4
koechslin <killian.oechslin@insa-lyon.fr>
2021-03-24 15:11:54 +0100

    fix: remove compilation warning

--- lexer.cpp
+++ lexer.cpp
@@ -23,7 +23,7 @@
 Symbole * Lexer::Consulter() {
     if (!tampon) {
 
-        if (tete == flux.length())
+        if (tete == (int)flux.length())
             tampon = new Symbole(FIN);
         else {
 
